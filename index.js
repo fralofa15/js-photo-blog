f00b9743e2feb9b225e7882374ede28cab4451f8
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-17 12:41:40 +0200

    - aggiunti link per bootstrap e css - aggiunta chiamata API

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
 
+
+fetch(API_URL)
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        console.log(json);
+    })
+    .catch(error => {
+        console.log(error);
+    })
