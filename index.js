dbd9f3dcb968b6de418ad563149df2d5933f9730
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-18 16:02:20 +0200

    Aggiunto in index.html: - header con il titolo - container per la pagina con le card - div per inserire le card Aggiunto in index.js: - ts-chek -const per il recupero del contenitore delle card tremite id - scrittura param per gli oggetti recuperati tremite la fetch

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// @ts-check
+'use strict';
 const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
+// Recupero il contenitore per le card
+const cardContainerElement = document.querySelector('#card-container');
 
+/**
+ * @param {{ id: number, title: string, date: string, url: string}[]} post
+ */
 
 fetch(API_URL)
     .then(response => {
