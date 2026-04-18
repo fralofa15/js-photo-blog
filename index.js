2c3a470b304a1c87d492b23a38ff932f4f3c4aa6
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-04-18 18:24:25 +0200

    Aggiunto in index.js: - funzione per generare le card - richiamo della funzione all'interno del .then

--- index.js
+++ index.js
@@ -8,12 +8,48 @@ const cardContainerElement = document.querySelector('#card-container');
  * @param {{ id: number, title: string, date: string, url: string}[]} post
  */
 
+function cardGenerator(post) {
+
+    //Controllo se cardContainerElement è null
+    if (cardContainerElement === null) {
+    console.log("Elemento non trovato");
+    return;
+}
+
+    // Ciclo for per generare le card
+    for (let i = 0; i < post.length; i++) {
+        const currentPost = post[i];
+
+        //Scrivo il codice HTML per la card
+        const cardHTML = `
+            <div class="col-12 col-md-6 col-lg-4 card-column">
+                <div class="photo-card" data-image="${currentPost.url}" data-title="${currentPost.title}">
+
+                    <div class="red-pin"></div>
+
+                    <div class="photo-container">
+                        <img src="${currentPost.url}" alt="${currentPost.title}">
+                    </div>
+
+                    <p class="photo-date">${currentPost.date}</p>
+                    <h2 class="photo-title">${currentPost.title}</h2>
+                </div>
+            </div>
+        `;
+        cardContainerElement.innerHTML += cardHTML;
+    }
+
+}
+
 fetch(API_URL)
     .then(response => {
         return response.json();
     })
     .then(json => {
         console.log(json);
+
+        // Chiamo la funzione per generare le card
+        cardGenerator(json);
     })
     .catch(error => {
         console.log(error);
